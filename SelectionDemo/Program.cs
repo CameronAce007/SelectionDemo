d9c6c39b2a924b6d1490c800128506dbc9e71833
CameronAce007 <CNkomo@ITLC7DKQV2>
2024-05-27 08:06:17 +0200

    Add project files.

---
+++ SelectionDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SelectionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter age");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 18) {
+                Console.WriteLine(" You and an adult");
+
+                if (age < 18)
+                {
+                    Console.WriteLine("You are a teenager");
+                }
+
+                if (age <= 0) {
+                    Console.WriteLine("Invalid age");
+                    }
+
+            }
+        }
+    }
+}
